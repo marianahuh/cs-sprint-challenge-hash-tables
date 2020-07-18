01f29ce1550298d25851e1aee5548f47ac34ee10
Mariana Huh <mhuh0176@gmail.com>
2020-07-17 20:24:41 -0700

    ex3 done

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -2,7 +2,18 @@ def intersection(arrays):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    cache = {}  # store numbers
+    result = []  # store intersected numbers in result list
+    nums = len(arrays)
+    for num_list in arrays:  # loop through each list in each array
+        for elm in num_list:  # loop through each element in list
+            if elm not in cache:  # if element is not cache, store to cache
+                cache[elm] = 1
+            else:
+                # otherwise,if it is in cache increment element by 1
+                cache[elm] += 1
+                if cache[elm] == nums:  # if elements in cache equals total amount in nums,
+                    result.append(elm)  # append the elements to result list
 
     return result
 
